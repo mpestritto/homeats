692c17fb2aed88de307615a52c68c0d49e917f13
Matt Pestritto <matt@pestritto.com>
2011-06-11 14:43:44 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Homeeat::Application.config.secret_token = 'e9bba9b145aba78c71da4d289a5209ac41947deb339799152e534441b2b649e75a99966334c36b980792a03e29dd6387dbba1c09bbc6aee9669276b9b901e45c'
