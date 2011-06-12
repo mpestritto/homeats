1235115c3a246ca0e26d7a1c223883d154dda816
Matt Pestritto <matt@pestritto.com>
2011-06-12 14:50:19 -0400

    added mailserver for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,6 @@ Homeeat::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.action_mailer.default_url_options = { :host => 'saucychefs.heroku.com' }
 end
