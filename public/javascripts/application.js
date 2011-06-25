2ac5a86e52028eeffd70219b76e1948159407717
Rao Venu <venuzr@gmail.com>
2011-06-25 12:44:36 -0400

    Got the watermart/hint text for email box to work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,13 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$(document).ready(function() {
+
+    ///Apply watermart text to email address textbox of beta_user_signup page
+    $('.reg_email_box').tbHinter({
+        text: 'Enter email address...',
+        class: 'email_greyText'
+    });
+
+});
+
